091408688dd6b58d271f020c9386e00858f16129
pinglun <455131058@qq.com>
2021-05-16 18:56:12 +0800

    first commit

---
+++ src/views/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout-container">
+
+    <router-view></router-view>
+
+    <!-- tabbar start -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">搜索</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+    <!-- tabbar end -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { active: 0 }
+  }
+}
+</script>
+
+<style></style>
